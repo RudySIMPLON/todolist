849ad29141bbefb5d0e791aec49f436fedf732af
lenni <lenni31@neuf.fr>
2016-10-11 11:55:34 +0200

    refonte du css html et javaxcript todo list v2

--- app.js
+++ app.js
@@ -1,65 +1,20 @@
-$('.lundi').click(function(){
-question = $('.lund').val();
-$('.tache1').append("<li>"+question+"</li>");
+alert('Bienvenue ! sur votre To Do List');
+console.log('voici mon texte afficher haha')
+
+$('.button').click(function(){
+	question = $('.input').val();
+	$('ul').append("<li>"+question+"</li>");
 
 });
 
-$('.mardi').click(function(){
-question = $('.mard').val();
-$('.tache2').append("<li>"+question+"</li>");
-
-});
-
-$('.mercredi').click(function(){
-question = $('.mercre').val();
-$('.tache3').append("<li>"+question+"</li>");
-
-});
-$('.jeudi').click(function(){
-question = $('.jeud').val();
-$('.tache4').append("<li>"+question+"</li>");
-
-});
-$('.vendredi').click(function(){
-question = $('.vendre').val();
-$('.tache5').append("<li>"+question+"</li>");
+$('ul').on('click','li', function(){
+	$(this).toggleClass('done');
 
 });
 
 
 
 
-
-$('.a').click(function(){
-$('.a').css('color','green');
-})
-$('.b').click(function(){
-$('.b').css('color','green');
-})
-$('.c').click(function(){
-$('.c').css('color','green');
-})
-$('.d').click(function(){
-$('.d').css('color','green');
-})
-$('.e').click(function(){
-$('.e').css('color','green');
-})
-$('.f').click(function(){
-$('.f').css('color','green');
-})
-$('.g').click(function(){
-$('.g').css('color','green');
-})
-$('.h').click(function(){
-$('.h').css('color','green');
-})
-$('.i').click(function(){
-$('.i').css('color','green');
-})
-$('.j').click(function(){
-$('.j').css('color','green');
-})
 
 
 
