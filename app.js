5b79f07a01658b394b934fadd0c6d8f8433a0295
lenni <lenni31@neuf.fr>
2016-10-12 10:34:42 +0200

    ajout des fonction entrer et de l'imposibiliter de cliquer sans valeur copyright

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-alert('Bienvenue ! sur votre To Do List');
-console.log('voici mon texte afficher haha')
+alert('Bienvenue ! sur ta To Do List, oublie pas de cliquer sur une tache pour la barré');
+console.log('voici mon texte afficher haha');
 
+$('input').keyup(function(e) { 
+	if(e.keyCode == 13) {
+		question = $('.input').val();
+		if ( question == ""){
+			$('ul').append();
+		} else {  
+			$('ul').append("<li>"+question+"</li>"); }
+			console.log(question); 
+		}
+	});
 $('.button').click(function(){
 	question = $('.input').val();
-	$('ul').append("<li>"+question+"</li>");
+	if ( question == ""){
+		$('ul').append();
+	} else {  
+		$('ul').append("<li>"+question+"</li>"); }
+		console.log(question); 
 
-});
+	});
 
 $('ul').on('click','li', function(){
 	$(this).toggleClass('done');
@@ -14,13 +28,15 @@ $('ul').on('click','li', function(){
 
 
 
+function effacer()
+{
+	document.getElementById('machin').value = '';
+}
 
+$("input").click(function(){
 
-
-
-
-
-
+	effacer();
+})
 
 
 
